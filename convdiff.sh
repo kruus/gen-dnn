ac8966bcce4affa106a676e13515b505d7450bcc
Erik Kruus <kruus@nec-labs.com>
2017-05-24 12:12:18 -0400

    redid upstream VANILLA conv mods (compiles)

---
+++ convdiff.sh
@@ -0,0 +1,7 @@
+diff -bBU2 src/cpu/jit_primitive_conf.hpp src/vanilla/any_primitive_conf.hpp
+diff -bBU2 src/cpu/gemm_convolution.hpp   src/vanilla/gemm_convolution.hpp
+diff -bBU2 src/cpu/gemm_convolution.cpp   src/vanilla/gemm_convolution.cpp
+diff -bBU2 src/cpu/gemm_convolution_utils.hpp   src/vanilla/gemm_convolution_utils.hpp
+diff -bBU2 src/cpu/gemm_convolution_utils.cpp   src/vanilla/gemm_convolution_utils.cpp
+
+
