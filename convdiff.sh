3ca1555bdeda62a1013d34d485f277e48d03b7bb
Erik Kruus <kruus@nec-labs.com>
2017-06-29 13:38:12 -0400

    vanilla/ identical files now symlink to cpu/
    This means I need to take care when editing vanilla/ stuff to
    not inadvertently change cpu/ files, which should mostly remain
    untouched except if intended for pushing to Intel github mkl-dnn.

--- convdiff.sh
+++ convdiff.sh
@@ -1,7 +1,7 @@
-diff -bBU2 src/cpu/jit_primitive_conf.hpp src/vanilla/any_primitive_conf.hpp
+diff -BbsU2 src/cpu/jit_primitive_conf.hpp src/vanilla/any_primitive_conf.hpp
 for v in `find src/vanilla`; do
 	c=src/cpu/`basename $v`
 	if [ -f "$v" -a -f "$c" ]; then
-		diff -bBU2 $c $v
+		diff -bBsU2 $c $v
 	fi
 done
