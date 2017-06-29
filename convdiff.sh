cd9552a8ebc53d179095725e91f8725cad401c57
Erik Kruus <kruus@nec-labs.com>
2017-06-29 13:53:06 -0400

    symlinks

--- convdiff.sh
+++ convdiff.sh
@@ -2,6 +2,7 @@ diff -BbsU2 src/cpu/jit_primitive_conf.hpp src/vanilla/any_primitive_conf.hpp
 for v in `find src/vanilla`; do
 	c=src/cpu/`basename $v`
 	if [ -f "$v" -a -f "$c" ]; then
+		if [ -h "$v" ]; then echo -n "Symlinked "; fi
 		diff -bBsU2 $c $v
 	fi
 done
