0d2bbb6db5437b1501e5c0baeebd289e4ed92475
Erik Kruus <ejkruus@gmail.com>
2017-10-26 23:27:44 -0400

    parens

--- tests/benchdnn/conv/bench_conv.cpp
+++ tests/benchdnn/conv/bench_conv.cpp
@@ -61,7 +61,7 @@ void reset_parameters() {
 bool check_mkldnn_support( const prb_t *p, res_t *res ) {
     char const *errmsg = nullptr;
     if ((p->dir == BWD_D || p->dir == BWD_W || p->dir == BWD_WB)
-            && p->dh>0 || p->dw>0) {
+            && (p->dh>0 || p->dw>0)) {
         printf(" dilation: p->dh=%d, p->dw=%d", p->dh, p->dw);
         errmsg="mkl-dnn BWD + dilation not possible (yet?)";
     }
