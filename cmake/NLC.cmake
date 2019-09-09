df59ed68a36d346f499f4400ed6c3197354aae8f
cdmalon <malon@nec-labs.com>
2019-09-10 05:07:40 +0900

    japan mods

--- cmake/NLC.cmake
+++ cmake/NLC.cmake
@@ -27,40 +27,53 @@ if(NECVE)
     # to try to automatically locate CBLAS for Aurora cross-compilation.
     #
     #set(NLCINC $ENV{NLC_BASE}/include)
-    find_path(NLCINC cblas.h NO_DEFAULT_PATH PATHS
-        /opt/nec/ve/nlc/1.0.0/include # latest version
-        /opt/nec/ve/nlc/0.9.0/include # older version from beta program
-        /usr/uhome/aurora/bm/nlc/0.9.0/include # ok for cross-compile on zoro
-        $ENV{NLC_BASE}
-        )
-
+    if(VE_CBLAS_INCLUDE_DIR)
+        set(NLCINC "${VE_CBLAS_INCLUDE_DIR}")
+    else()
+        find_path(NLCINC cblas.h NO_DEFAULT_PATH PATHS
+            ${NLC_HOME}
+            /opt/nec/ve/nlc/1.0.0/include # latest version
+            /opt/nec/ve/nlc/0.9.0/include # older version from beta program
+            /usr/uhome/aurora/bm/nlc/0.9.0/include # ok for cross-compile on zoro
+            $ENV{NLC_BASE}
+            )
+    endif()
     if(NOT NLCINC)
         message(STATUS "Aurora NLC cblas not found")
         return()
     endif()
-    get_filename_component(NLCROOT "${NLCINC}" PATH)
+
+    if(NLC_HOME)
+        set(NLCROOT "${NLC_HOME}")
+    else()
+        get_filename_component(NLCROOT "${NLCINC}" PATH)
+    endif()
     message(STATUS "NLCROOT = ${NLCROOT}")
     message(STATUS "Using Aurora NLC cblas found under ${NLCROOT}")
 
-    find_library(NLCLIB NAMES cblas PATHS ${NLCROOT}/lib)
-    if(NLCLIB)
-        #set(NLCLIB $ENV{NLC_BASE}/lib/libcblas.a)
+    if(VE_NLC_CBLAS_LIB_PATHS)
+        set(NLCLIB ${VE_NLC_CBLAS_LIB_PATHS)
+    else()
+        find_library(NLCLIB NAMES cblas PATHS ${NLCROOT}/lib)
     endif()
     message(STATUS "NLCLIB = ${NLCLIB}")
 
     #set(NLCLIBBLAS $ENV{NLC_BASE}/lib/libblas_pthread.a)
     #set(NLCLIBBLAS /opt/nec/ve/nlc/0.9.0/lib/libblas_sequential.a)
     # find_library(NLCLIBBLAS NAMES blas_pthread PATHS ${NLCROOT}/lib)
-    find_library(NLCLIBBLAS NAMES blas_sequential PATHS ${NLCROOT}/lib)
-    if(NLCLIBBLAS)
-        #set(NLCLIBBLAS $ENV{NLC_BASE}/lib/libcblas_pthread.a)
+    if(VE_NLC_CBLAS_LIB_PATHS)
+        set(NLCLIBBLAS ${VE_NLC_VBLAS_LIB_PATHS})
+    else()
+        find_library(NLCLIBBLAS NAMES blas_sequential PATHS ${NLCROOT}/lib)
     endif()
     message(STATUS "NLCLIBBLAS = ${NLCLIBBLAS}")
 
     if(NLCLIB AND NLCLIBBLAS)
         add_definitions(-DUSE_CBLAS)
         include_directories(AFTER ${NLCINC})
+        #include_directories(AFTER ${VE_NLC_INCLUDES})
         list(APPEND mkldnn_LINKER_LIBS ${NLCLIB} ${NLCLIBBLAS})
+        #list(APPEND mkldnn_LINKER_LIBS ${VE_NLC_CBLAS_LIB_PATHS})
     endif()
 endif()
 # vim: et ts=4 sw=4 ai
