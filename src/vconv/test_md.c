23684899a2412f490cc2b663133964a8c5a82a71
Erik Kruus <kruus@nec-labs.com>
2019-09-19 02:01:04 +0900

    ve version of vconv/test_md

--- src/vconv/test_md.c
+++ src/vconv/test_md.c
@@ -1,4 +1,9 @@
+/** Copyright 2019 NEC Labs America
+ * see LICENSE file */
+/** \file
+ * \c mkldnn_memory_desc_init demo. */
 #include "mkldnn_desc_init.h"
+#include "mkldnn_debug.h"
 #include <stdio.h>
 
 static inline void md_0xFF( mkldnn_memory_desc_t *md ){
@@ -54,6 +59,34 @@ static void print_md_full( mkldnn_memory_desc_t *md ){
     printf("}");
     fflush(stdout);
 }
+mkldnn_status_t mkmd(int const ndims, mkldnn_memory_format_t const fmt,
+        char const* msg)
+{    
+    int const verbose=1;
+    mkldnn_data_type_t dt=mkldnn_f32;
+#if 0
+    int const dims[12]={1,2,3,4,5,6,7,8,9,10,11,12};
+#else
+    int dims[(ndims<0? 0: ndims)];
+    for(int i=0; i<ndims; ++i) dims[i]=i;
+#endif
+    if(verbose>0){
+        printf("\n=== md_0xFF; %d-dim %s",ndims,mkldnn_fmt2str(fmt));
+        if(msg) printf(" : %s",msg);
+        printf("\n");
+    }
+    mkldnn_memory_desc_t md;
+    md_0xFF(&md);
+    if(verbose>1)
+        printf(" about to call memory_desc_init\n"); fflush(stdout);
+    mkldnn_status_t s = mkldnn_memory_desc_init(&md,
+            ndims, dims, dt, fmt);
+    if(s != mkldnn_success) printf("ERROR: status = %d\n",s);
+    if(verbose>1)
+        printf(" back from memory_desc_init\n"); fflush(stdout);
+    print_md_full(&md);
+    return s;
+}
 int main(int argc, char** argv){
     if(1){
         printf("\n=== md_0xFF\n");
@@ -61,25 +94,12 @@ int main(int argc, char** argv){
         md_0xFF(&md);
         print_md_full(&md);
     }
-    if(1){
-        printf("\n=== md_0xFF, then 'x' 1D tensor\n");
-        mkldnn_memory_desc_t md;
-        md_0xFF(&md);
-        mkldnn_memory_format_t fmt = mkldnn_x;
-        int ndims=1; // note: fmt and ndims MUST be compatible
-        int dims[12]={1,2,3,4,5,6,7,8,9,10,11,12};
-        //int dims[12]={1,0};
-        mkldnn_data_type_t dt=mkldnn_f32;
-        printf(" about to call memory_desc_init\n"); fflush(stdout);
-        mkldnn_status_t s = mkldnn_memory_desc_init(&md, ndims, dims, dt, fmt );
-        if(s != mkldnn_success){
-            printf("ERROR: status = %d\n",s);
-        }
-        printf(" back from memory_desc_init\n"); fflush(stdout);
-        print_md_full(&md);
-    }
+    mkmd(666/*don't care*/, mkldnn_format_undef,""); // zero_md(), all zeros (no garbage)
+    mkmd(1, mkldnn_x,"");
+    mkmd(4, mkldnn_nchw,"");
+    //mkmd(0, mkldnn_x,""); // acceptable! returns zero_md() [all-zeroes]
+    mkmd(0, mkldnn_nchw,""); // zero_md() [all-zeros]
     printf("\nGoodbye");
     return 0;
 }
-
-
+/* vim: set et ts=4 sw=4 cino=^=l0,\:0,N-s: */
